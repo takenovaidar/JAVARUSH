7abe760be084e17837964cd7d696b5349e434e9c
Aidar Takenov <aidartaken@gmail.com>
2024-12-25 22:56:33 +0600

    Multi array
    In this task I should just print the given three-dimensional array. For that I used three loops for each, so I learned how to print three or more dimensional arrays.

---
+++ JavaRush_3.java
@@ -0,0 +1,21 @@
+
+/* 
+Создаем мультимассив
+*/
+
+public class JavaRush_3 {
+
+    public static int[][][] multiArray = new int[][][]{{{4, 8, 15}, {16}}, {{23, 42}, {}}, {{1}, {2}, {3}, {4, 5}}};
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for(int i = 0; i < multiArray.length; i++) {
+            for (int j = 0; j < multiArray[i].length; j++) {
+                for (int k = 0; k < multiArray[i][j].length; k++) {
+                    System.out.print(multiArray[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
