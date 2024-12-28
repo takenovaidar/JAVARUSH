88799c3789c8b38c565ca575a8623a0b3041886c
Aidar Takenov <aidartaken@gmail.com>
2024-12-28 18:48:27 +0600

    Methods(Reversing)
    In general, right now I'm learning about methods. This task is also about methods, but the method is already created(reverseArray method). So my task is to fill that method so that indexes  of array would have opposite order.

---
+++ JavaRush_6.java
@@ -0,0 +1,30 @@
+/*
+The right order
+*/
+
+public class JavaRush_6 {
+
+    public static void main(String[] args) {
+        int[] array = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        //write your code here
+        for(int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
+
