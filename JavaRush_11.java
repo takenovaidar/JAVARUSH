6e6831dbc8bf8375499ff963cb60760291662cfd
Aidar Takenov <aidartaken@gmail.com>
2024-12-31 23:38:06 +0600

    Keyword this
    In this little task I learned how to use this keyword. The program won't give the output, propose was to just explain syntax of this keyword.

---
+++ JavaRush_11.java
@@ -0,0 +1,15 @@
+public class JavaRush_11 {
+    public String name = "Amigo";
+    public String position = "Java developer";
+    public int salary = 10_000;
+
+    public void setPosition(String position) {
+        position = position;
+        this.position = position;
+    }
+
+    public void setSalary(int salary) {
+        salary = salary;
+        this.salary = salary;
+    }
+}
