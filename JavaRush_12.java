253991581b09227d7d8dcf888bde5838b4b8dd8b
Aidar Takenov <aidartaken@gmail.com>
2025-01-01 23:13:17 +0600

    Maximum and minimum
    Today it's was the next task as always but it was themes that I've learned.

---
+++ JavaRush_12.java
@@ -0,0 +1,7 @@
+public class JavaRush_12 {
+    //напишите тут ваш код
+    byte byteMax = Byte.MAX_VALUE;       // Максимальное значение для byte
+    short shortMax = Short.MAX_VALUE;   // Максимальное значение для short
+    int intMax = Integer.MAX_VALUE;     // Максимальное значение для int
+    long longMax = Long.MAX_VALUE;
+}
