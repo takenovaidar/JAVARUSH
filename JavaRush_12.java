98c1183e98d76cb89ddea3f4fe257d4d5975492f
Aidar Takenov <aidartaken@gmail.com>
2025-01-01 23:16:15 +0600

    JavaRush_12.java
    I've added minimums of types. But these were the themes that I've learned month ago... But I did it anyway because these are the tasks of JavaRush as well.

--- JavaRush_12.java
+++ JavaRush_12.java
@@ -4,4 +4,9 @@ public class JavaRush_12 {
     short shortMax = Short.MAX_VALUE;   // Максимальное значение для short
     int intMax = Integer.MAX_VALUE;     // Максимальное значение для int
     long longMax = Long.MAX_VALUE;
+    //adding minimums
+    byte byteMin = Byte.MIN_VALUE;    
+    short shortMin = Short.MIN_VALUE;   
+    int intMin = Integer.MIN_VALUE;     
+    long longMin = Long.MIN_VALUE;
 }
