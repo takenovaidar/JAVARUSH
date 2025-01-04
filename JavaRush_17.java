5eb6952ead631ef71b5aa22ff102779643602ce3
Aidar Takenov <aidartaken@gmail.com>
2025-01-04 23:01:09 +0600

    Objects
    In this task I was had to create ten String objects.

---
+++ JavaRush_17.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class JavaRush_17 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String s1 = new String("Aaa");
+        String s2 = new String("Aaa");
+        String s3 = new String("Aaa");
+        String s4 = new String("Aaa");
+        String s5 = new String("Aaa");
+        String s6 = new String("Aaa");
+        String s7 = new String("Aaa");
+        String s8 = new String("Aaa");
+        String s9 = new String("Aaa");
+        String s10 = new String("Aaa");
+    }
+}
