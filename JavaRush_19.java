55616b38e5a6128419c3ac2786e1246ce4dc8895
Aidar Takenov <aidartaken@gmail.com>
2025-01-08 21:04:34 +0600

    Class Math
    Today I learned how to use some of the math classes.

---
+++ JavaRush_19.java
@@ -0,0 +1,16 @@
+import static java.lang.Math.*;
+
+public class JavaRush_19 {
+    public static double sin(double a) {
+        return Math.sin(toRadians(a));
+    }
+
+    public static double cos(double a) {
+        return Math.cos(toRadians(a));
+    }
+
+    public static double tan(double a) {
+        return Math.tan(toRadians(a));
+    }
+}
+
