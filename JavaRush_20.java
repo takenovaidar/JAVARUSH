7364c9811ef3a3947b1463b8b22e8dfc7d583631
Aidar Takenov <aidartaken@gmail.com>
2025-01-10 23:34:59 +0600

    Math class 2

---
+++ JavaRush_20.java
@@ -0,0 +1,14 @@
+public class JavaRush_20 {
+
+    public static double sqrt(double a) {
+        return Math.sqrt(a);
+    }
+
+    public static double cbrt(double a) {
+        return Math.cbrt(a);
+    }
+
+    public static double pow(int number, int power) {
+        return Math.pow(number, power);
+    }
+}
