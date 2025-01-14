3bf4bfb269c05b858fa40eaa1a7ed6cdc7a0f111
Aidar Takenov <aidartaken@gmail.com>
2025-01-14 23:14:15 +0600

    Math return
    Today I learned about Math.return class. In this task I was had to print random integers from 0 to 100.

---
+++ JavaRush_22.java
@@ -0,0 +1,12 @@
+public class JavaRush_22 {
+
+    public static void main(String[] args) {
+        System.out.println(generateNumber());
+    }
+
+    public static int generateNumber() {
+        //напишите тут ваш код
+        int x = (int) (Math.random() * 100);
+        return x;
+    }
+}
