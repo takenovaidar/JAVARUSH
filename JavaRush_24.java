c8b856bf9092ad25eea225213e3e7cd2f7e860df
Aidar Takenov <aidartaken@gmail.com>
2025-01-15 22:49:14 +0600

    Imports
    Here is a task, I have to delete the imports and write them into the JavaRush_24 class.

---
+++ JavaRush_24.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+/*
+Импорты: Part 1
+*/
+
+public class JavaRush_24 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new StringReader(""));
+        BigInteger bigInteger = new BigInteger("123");
+    }
+}
