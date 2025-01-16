9eaa866f1b0a006c691a34774af6b69f9b6d866b
Aidar Takenov <aidartaken@gmail.com>
2025-01-16 23:45:17 +0600

    Imports 2
    Here I have to fix the imports.

---
+++ JavaRush_25.java
@@ -0,0 +1,15 @@
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/*
+Импорты: Part 2
+*/
+
+public class JavaRush_25 {
+    public static void main(String[] args) {
+        java.util.LinkedList<java.lang.String> linkedList = new java.util.LinkedList<>();
+        java.io.InputStreamReader inputStreamReader = new java.io.InputStreamReader(java.lang.System.in);
+        java.util.concurrent.atomic.AtomicBoolean atomicBoolean = new java.util.concurrent.atomic.AtomicBoolean(false);
+    }
+}
