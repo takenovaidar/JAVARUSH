aaacde009068a5d025d67498d191aa77731eb257
Aidar Takenov <aidartaken@gmail.com>
2025-01-17 23:15:54 +0600

    Thread
    Here I have to solve this task.

---
+++ JavaRush_26.java
@@ -0,0 +1,16 @@
+/*
+Обратный отсчет
+*/
+
+public class JavaRush_26 {
+
+    public static void main(String[] args) throws InterruptedException {
+        countDown();
+    }
+    public static void countDown() throws InterruptedException {
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("Старт!");
+    }
+}
