393bae5726e90298986faa403f228a8cf53a01c6
Aidar Takenov <aidartaken@gmail.com>
2025-01-19 23:36:28 +0600

    Bit-by-bit operations
    In this task I was had to print an integer 5, by using only Two comments. So the code already was given as always. To sum up, I'm learning how to use bit operations.

--- JavaRush_29.java
+++ JavaRush_29.java
@@ -2,9 +2,9 @@ public class JavaRush_29 {
 
     public static void main(String[] args) {
         int x = 7;
-        x = x & 25;
+        //x = x & 25;
         x = x & 5;
-        x = x & 3;
+        //x = x & 3;
         x = x & 12;
         x = x | 1;
         System.out.println(x);
