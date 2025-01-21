68ad8128ce0f7af8adaa8371fa4caec936872165
Aidar Takenov <aidartaken@gmail.com>
2025-01-21 23:00:30 +0600

    Flags
    Here is the task:

---
+++ JavaRush_32.java
@@ -0,0 +1,17 @@
+public class JavaRush_32 {
+
+    public static int setFlag(int number, int flagPos) {
+        //напишите тут ваш код
+        return 0;
+    }
+
+    public static int resetFlag(int number, int flagPos) {
+        //напишите тут ваш код
+        return 0;
+    }
+
+    public static boolean checkFlag(int number, int flagPos) {
+        //напишите тут ваш код
+        return false;
+    }
+}
