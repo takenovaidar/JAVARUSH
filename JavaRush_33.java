eb0e4e63cc8301deb7fdf78365ce6ab39d5f9ba1
Aidar Takenov <aidartaken@gmail.com>
2025-01-22 23:21:01 +0600

    Priorities
    In this task I had to solve the equation with an order to get 12. Also I've learned other orders of the operations just like in math.

---
+++ JavaRush_33.java
@@ -0,0 +1,11 @@
+/*
+Приоритеты
+*/
+
+public class JavaRush_33 {
+
+    public static void main(String[] args) {
+        int number = 2;
+        System.out.println(number + number * (number + ++number));
+    }
+}
