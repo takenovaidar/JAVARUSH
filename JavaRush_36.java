998b44e41045b79521a545df77f5a920a854bbc8
Aidar Takenov <aidartaken@gmail.com>
2025-01-23 22:57:05 +0600

    Literals 2
    Here I had to solve the task by fixing the given int variables. In this task also I was not allowed to change the numbers, I had to just change ints to double or float.

---
+++ JavaRush_36.java
@@ -0,0 +1,23 @@
+/*
+Литералы с плавающей точкой
+*/
+
+public class JavaRush_36 {
+    public static double a = 0.;
+    public static double b = .0;
+    public static double c = 100D;
+    public static double d = 100.0;
+    public static double e = 1.11E5;
+    public static float f = 200F;
+    public static float g = 0.F;
+
+    public static void main(String[] args) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
