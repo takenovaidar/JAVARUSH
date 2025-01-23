1f098a61847e94541f6f14e17e223a0cb2fb8d3e
Aidar Takenov <aidartaken@gmail.com>
2025-01-23 22:54:43 +0600

    Literals
    Here I had to solve the task by fixing the meanings if variables. But I was not allowed change the integer, I could only change the symbols for each variable type.

---
+++ JavaRush_35.java
@@ -0,0 +1,17 @@
+/*
+Целочисленные литералы
+*/
+
+public class JavaRush_35 {
+    public static byte b = -128;
+    public static short s = -32768;
+    public static int i = 1_234_567_890;
+    public static long l = 2_345_678_900L;
+
+    public static void main(String[] args) {
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+    }
+}
