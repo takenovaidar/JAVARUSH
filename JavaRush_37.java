06ea92bdd1fcf4591245ba6bc4d7c02083a6b1b8
Aidar Takenov <aidartaken@gmail.com>
2025-01-24 21:44:29 +0600

    Literals 3
    Here is updated solution, I just added new variables related to literals, and with their help I printed the sentences and a word "Java".

--- JavaRush_37.java
+++ JavaRush_37.java
@@ -3,9 +3,21 @@
 */
 
 public class JavaRush_37 {
-    public String text = "Всегда пишите код так, " +
+    public static String text = "Всегда пишите код так, " +
             "будто сопровождать его будет " +
             "склонный к насилию психопат, " +
             "который знает, где вы живете. " +
             "Martin Golding";
+    public static char first = '\u004A';
+    public static char second = 'a';
+    public static char third = '\u0076';
+    public static char fourth = 'a';
+
+    public static void main(String[] args) {
+        System.out.println(text);
+        System.out.print(first);
+        System.out.print(second);
+        System.out.print(third);
+        System.out.print(fourth);
+    }
 }
