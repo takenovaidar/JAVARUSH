f70648a04773c4cdf018a3ff9e20015d429f45a2
Aidar Takenov <aidartaken@gmail.com>
2025-01-25 22:19:27 +0600

    Eightfold coding
    Here  I have to solve the task by turning the octal number to decimal, and decimal to octal. I have to complete the given methods.

---
+++ JavaRush_39.java
@@ -0,0 +1,18 @@
+public class JavaRush_39 {
+    public static void main(String[] args) {
+        int decimalNumber = 21;
+        System.out.println("Десятичное число " + decimalNumber + " равно восьмеричному числу " + toOctal(decimalNumber));
+        int octalNumber = 25;
+        System.out.println("Восьмеричное число " + octalNumber + " равно десятичному числу " + toDecimal(octalNumber));
+    }
+
+    public static int toOctal(int decimalNumber) {
+        //напишите тут ваш код
+        return 0;
+    }
+
+    public static int toDecimal(int octalNumber) {
+        //напишите тут ваш код
+        return 0;
+    }
+}
