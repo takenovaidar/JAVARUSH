7ab2ce7c163e21fefb761b7ffd6e2c6556889619
Aidar Takenov <aidartaken@gmail.com>
2025-01-25 22:53:57 +0600

    Eightful coding
    In this task I had sole the task by turning octal number to decimal number, and decimal to octal.  I had complete methods.

--- JavaRush_39.java
+++ JavaRush_39.java
@@ -7,12 +7,30 @@ public class JavaRush_39 {
     }
 
     public static int toOctal(int decimalNumber) {
-        //напишите тут ваш код
-        return 0;
+        int octalNumber = 0;
+        if (decimalNumber <= 0) {
+            return octalNumber;
+        }
+
+        int i = 0;
+        while (decimalNumber != 0) {
+            octalNumber = (int) (octalNumber + (decimalNumber % 8) * Math.pow(10, i++));
+            decimalNumber = decimalNumber / 8;
+        }
+        return octalNumber;
     }
 
     public static int toDecimal(int octalNumber) {
-        //напишите тут ваш код
-        return 0;
+        int decimalNumber = 0;
+        if (octalNumber <= 0) {
+            return decimalNumber;
+        }
+
+        int i = 0;
+        while (octalNumber != 0) {
+            decimalNumber = (int) (decimalNumber + (octalNumber % 10) * Math.pow(8, i++));
+            octalNumber = octalNumber / 10;
+        }
+        return decimalNumber;
     }
 }
