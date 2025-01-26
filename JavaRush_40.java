6c1bf1edecbc89453aad48ea6cf38bc532467903
Aidar Takenov <aidartaken@gmail.com>
2025-01-26 23:08:45 +0600

    Binary code
    Here is the solution of the task, In this task I had to turn binary number to decimal number, and decimal to binary.

--- JavaRush_40.java
+++ JavaRush_40.java
@@ -1,5 +1,3 @@
-import java.util.regex.Pattern;
-
 /*
 Двоичный конвертер
 */
@@ -13,12 +11,29 @@ public class JavaRush_40 {
     }
 
     public static String toBinary(int decimalNumber) {
-        //напишите тут ваш код
-        return null;
+        String binaryNumber = "";
+        if (decimalNumber <= 0) {
+            return binaryNumber;
+        }
+
+        while (decimalNumber != 0) {
+            binaryNumber = decimalNumber % 2 + binaryNumber;
+            decimalNumber = decimalNumber / 2;
+        }
+        return binaryNumber;
     }
 
     public static int toDecimal(String binaryNumber) {
-        //напишите тут ваш код
-        return 0;
+        int decimalNumber = 0;
+        if (binaryNumber == null) {
+            return decimalNumber;
+        }
+
+        for (int i = 0; i < binaryNumber.length(); i++) {
+            int index = binaryNumber.length() - 1 - i;
+            int value = Character.getNumericValue(binaryNumber.charAt(index));
+            decimalNumber += (int) (value * Math.pow(2, i));
+        }
+        return decimalNumber;
     }
 }
