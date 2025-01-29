ebea7ea4efb07ff92dc52706955b2879d91eb5df
Aidar Takenov <aidartaken@gmail.com>
2025-01-29 21:58:31 +0600

    Hex number 2

---
+++ JavaRush_42.java
@@ -0,0 +1,23 @@
+/*
+Двоично-шестнадцатеричный конвертер
+*/
+
+public class JavaRush_42 {
+
+    public static void main(String[] args) {
+        String binaryNumber = "100111010000";
+        System.out.println("Двоичное число " + binaryNumber + " равно шестнадцатеричному числу " + toHex(binaryNumber));
+        String hexNumber = "9d0";
+        System.out.println("Шестнадцатеричное число " + hexNumber + " равно двоичному числу " + toBinary(hexNumber));
+    }
+
+    public static String toHex(String binaryNumber) {
+        //напишите тут ваш код
+        return null;
+    }
+
+    public static String toBinary(String hexNumber) {
+        //напишите тут ваш код
+        return null;
+    }
+}
